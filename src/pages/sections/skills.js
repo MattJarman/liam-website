ce792596fdf8383014c1cde2935374f8fe79f65f
Matt <mattjarman22@outlook.com>
2021-11-26 14:55:56 +0000

    Comment out skills

--- src/pages/sections/skills.js
+++ src/pages/sections/skills.js
@@ -50,42 +50,42 @@ const SkillsSection = () => {
         className="container px-5 py-24 mx-auto flex flex-col lg:w-4/6 lg:mx-auto"
       >
         <div className="grid gap-y-2 grid-cols-1 md:grid-cols-none md:grid-rows-6 md:grid-flow-col xl:gap-y-4">
-          {skills.map(({ node }, index) => {
-            const { id, name, level } = node
-            return (
-              <div
-                key={id}
-                className="flex flex-row items-center justify-center"
-              >
-                <motion.div
-                  custom={index}
-                  initial={{ opacity: 0, x: 20 }}
-                  animate={iconControls}
-                  className="flex items-center justify-center mr-4 shadow-md p-2 rounded-md w-12 h-12 xl:w-16 xl:h-16"
-                >
-                  <Icon name={name} />
-                </motion.div>
-                <motion.div
-                  custom={index}
-                  initial={{ opacity: 0, x: 20 }}
-                  animate={barBackgroundControls}
-                  className="w-full bg-gray-100 mr-4 shadow-lg border-2 border-green-400 rounded-sm"
-                >
-                  <div style={{ width: `${level}%` }}>
-                    <motion.div
-                      custom={index}
-                      ref={skillsRef}
-                      initial={{ opacity: 0, width: 0 }}
-                      animate={barControls}
-                      className="h-6 xl:h-8 shadow-none flex flex-col font-semibold uppercase px-2 text-gray-800 justify-center bg-green-400 text-xs tracking-wider antialiased"
-                    >
-                      {name}
-                    </motion.div>
-                  </div>
-                </motion.div>
-              </div>
-            )
-          })}
+          {/*{skills.map(({ node }, index) => {*/}
+          {/*  const { id, name, level } = node*/}
+          {/*  return (*/}
+          {/*    <div*/}
+          {/*      key={id}*/}
+          {/*      className="flex flex-row items-center justify-center"*/}
+          {/*    >*/}
+          {/*      <motion.div*/}
+          {/*        custom={index}*/}
+          {/*        initial={{ opacity: 0, x: 20 }}*/}
+          {/*        animate={iconControls}*/}
+          {/*        className="flex items-center justify-center mr-4 shadow-md p-2 rounded-md w-12 h-12 xl:w-16 xl:h-16"*/}
+          {/*      >*/}
+          {/*        <Icon name={name} />*/}
+          {/*      </motion.div>*/}
+          {/*      <motion.div*/}
+          {/*        custom={index}*/}
+          {/*        initial={{ opacity: 0, x: 20 }}*/}
+          {/*        animate={barBackgroundControls}*/}
+          {/*        className="w-full bg-gray-100 mr-4 shadow-lg border-2 border-green-400 rounded-sm"*/}
+          {/*      >*/}
+          {/*        <div style={{ width: `${level}%` }}>*/}
+          {/*          <motion.div*/}
+          {/*            custom={index}*/}
+          {/*            ref={skillsRef}*/}
+          {/*            initial={{ opacity: 0, width: 0 }}*/}
+          {/*            animate={barControls}*/}
+          {/*            className="h-6 xl:h-8 shadow-none flex flex-col font-semibold uppercase px-2 text-gray-800 justify-center bg-green-400 text-xs tracking-wider antialiased"*/}
+          {/*          >*/}
+          {/*            {name}*/}
+          {/*          </motion.div>*/}
+          {/*        </div>*/}
+          {/*      </motion.div>*/}
+          {/*    </div>*/}
+          {/*  )*/}
+          {/*})}*/}
         </div>
       </div>
     </section>
